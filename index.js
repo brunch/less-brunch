574b4d093c2b5f298748d40c3a56481ca2d2e74c
bvauchelle <bvauchelle@kds.com>
2015-01-08 17:26:22 +0100

    Beautified javascript

--- index.js
+++ index.js
@@ -19,23 +19,24 @@ LESSCompiler.prototype.extension = 'less';
 LESSCompiler.prototype.compile = function(params, callback) {
   var path = params.path;
   var data = params.data;
- 
-  less.render(data
-    , {
-        paths: [this.rootPath, sysPath.dirname(path)].concat(this.config.paths || []),  
-        filename: path, 
-        dumpLineNumbers: !this.optimize && this.config.dumpLineNumbers 
-    }, function(error, output) {
-        if (error != null) {
-            var err;
-            err = '' + error.type + 'Error:' + error.message;
-            if (error.filename) {
-                err += ' in "' + error.filename + ':' + error.line + ':' + error.column + '"';
-            }
-            return callback(err);
-        }
-        return callback(error, {data: output.css});
+
+  less.render(data, {
+    paths: [this.rootPath, sysPath.dirname(path)].concat(this.config.paths || []),
+    filename: path,
+    dumpLineNumbers: !this.optimize && this.config.dumpLineNumbers
+  }, function(error, output) {
+    if (error != null) {
+      var err;
+      err = '' + error.type + 'Error:' + error.message;
+      if (error.filename) {
+        err += ' in "' + error.filename + ':' + error.line + ':' + error.column + '"';
+      }
+      return callback(err);
+    }
+    return callback(error, {
+      data: output.css
     });
+  });
 };
 
 module.exports = LESSCompiler;
