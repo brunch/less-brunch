4b58009e1c3f1fece47d29b26f962b42f37a8530
Hugo Wood <hugo.wood-ext@vif.fr>
2016-09-09 09:30:47 +0200

    Make tests respect eslint rules

--- test.js
+++ test.js
@@ -1,24 +1,27 @@
+/* eslint-env mocha */
+/* eslint no-unused-expressions: off */
+
 var fs = require('fs');
 var path = require('path');
 var expect = require('chai').expect;
 var Plugin = require('./');
 
-describe('Plugin', function() {
+describe('Plugin', () => {
   var plugin;
 
-  beforeEach(function() {
+  beforeEach(() => {
     plugin = new Plugin({paths: {root: '.'}});
   });
 
-  it('should be an object', function() {
+  it('should be an object', () => {
     expect(plugin).to.be.ok;
   });
 
-  it('should has #compile method', function() {
+  it('should has #compile method', () => {
     expect(plugin.compile).to.be.an.instanceof(Function);
   });
 
-  it('should compile and produce valid result', function(done) {
+  it('should compile and produce valid result', (done) => {
     var content = '@color: #4D926F; #header {color: @color;}';
     var expected = '#header {\n  color: #4D926F;\n}\n';
 
@@ -28,7 +31,7 @@ describe('Plugin', function() {
     }, error => expect(error).not.to.be.ok);
   });
 
-  it('should handle invalid less gracefully', function(done) {
+  it('should handle invalid less gracefully', (done) => {
     var content = '#header {color: @color;}';
     var expected = 'NameError:variable @color is undefined in "style.less:1:16"';
 
@@ -38,7 +41,7 @@ describe('Plugin', function() {
     });
   });
 
-  it('should correctly identify stylesheet and data-uri dependencies', function(done) {
+  it('should correctly identify stylesheet and data-uri dependencies', (done) => {
     var file = 'test-files/test-dependency-resolution.less';
     var content = fs.readFileSync(path.join(__dirname, file));
 
