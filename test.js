f2efde09a559cbddf8d63a766a8c3fcbf4671bcd
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-10-23 07:41:53 +0300

    Update & refactor tests

--- test.js
+++ test.js
@@ -1,57 +1,62 @@
 /* eslint-env mocha */
 /* eslint no-unused-expressions: off */
 
-var fs = require('fs');
-var path = require('path');
-var expect = require('chai').expect;
-var Plugin = require('./');
+'use strict';
+
+const fs = require('fs');
+const join = require('path').join;
+const expect = require('chai').expect;
+const LESSCompiler = require('./');
 
 describe('Plugin', () => {
-  var plugin;
+  let plugin;
 
   beforeEach(() => {
-    plugin = new Plugin({paths: {root: '.'}});
-  });
-
-  it('should be an object', () => {
-    expect(plugin).to.be.ok;
-  });
-
-  it('should has #compile method', () => {
-    expect(plugin.compile).to.be.an.instanceof(Function);
-  });
-
-  it('should compile and produce valid result', (done) => {
-    var content = '@color: #4D926F; #header {color: @color;}';
-    var expected = '#header {\n  color: #4D926F;\n}\n';
-
-    plugin.compile({data: content, path: 'style.less'}).then(result => {
-      expect(result.data).to.equal(expected);
-      done();
-    }, error => expect(error).not.to.be.ok);
-  });
-
-  it('should handle invalid less gracefully', (done) => {
-    var content = '#header {color: @color;}';
-    var expected = 'NameError:variable @color is undefined in "style.less:1:16"';
-
-    plugin.compile({data: content, path: 'style.less'}).then(null, error => {
-      expect(error).to.equal(expected);
-      done();
+    plugin = new LESSCompiler({
+      paths: {root: '.'},
+      plugins: {},
     });
   });
 
-  it('should correctly identify stylesheet and data-uri dependencies', (done) => {
-    var file = 'test-files/test-dependency-resolution.less';
-    var content = fs.readFileSync(path.join(__dirname, file));
+  it('should be an object', () => {
+    expect(plugin).to.be.an.instanceof(LESSCompiler);
+  });
 
+  it('should have #compile method', () => {
+    expect(plugin).to.respondTo('compile');
+  });
 
-    plugin.getDependencies({data: content, path: file}).then(deps => {
-      expect(deps).to.eql([
-        path.join('test-files', 'test-include.less'),
-        path.join('test-files', 'img', 'foo.jpg')
+  it('should have #getDependencies method', () => {
+    expect(plugin).to.respondTo('getDependencies');
+  });
+
+  it('should compile and produce valid result', () => {
+    const data = '@color: #4D926F; #header {color: @color;}';
+    const expected = '#header {\n  color: #4D926F;\n}\n';
+
+    return plugin.compile({data, path: 'style.less'}).then(result => {
+      expect(result.data).to.equal(expected);
+    });
+  });
+
+  it('should handle invalid less gracefully', () => {
+    const data = '#header {color: @color;}';
+    const expected = 'NameError: variable @color is undefined in "style.less:1:16"';
+
+    return plugin.compile({data, path: 'style.less'}).catch(error => {
+      expect(error).to.equal(expected);
+    });
+  });
+
+  it('should correctly identify stylesheet and data-uri dependencies', () => {
+    const path = 'test-files/test-dependency-resolution.less';
+    const data = fs.readFileSync(path, 'utf-8');
+
+    return plugin.getDependencies({data, path}).then(deps => {
+      expect(deps).to.deep.equal([
+        join('test-files', 'test-include.less'),
+        join('test-files', 'img', 'foo.jpg'),
       ]);
-      done();
     });
   });
 });
