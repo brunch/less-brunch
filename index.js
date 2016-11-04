990ab000e1a7af0f38d7b5c6f2ef8aa7ba605ed8
Julian Lyndon-Smith <julian+cloud9@dotr.com>
2016-11-04 20:36:48 +0000

    added ignore option to less plugin

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const progeny = require('progeny');
 
 const postcss = require('postcss');
 const postcssModules = require('postcss-modules');
+const anymatch = require('anymatch');
 
 const cssModulify = (path, data, map) => {
   let json = {};
@@ -24,6 +25,9 @@ class LESSCompiler {
     this.optimize = config.optimize;
 
     this.modules = this.config.modules || this.config.cssModules;
+    
+    this.isIgnored = anymatch(this.config.ignore);
+    
     delete this.config.modules;
     delete this.config.cssModules;
   }
@@ -52,6 +56,8 @@ class LESSCompiler {
       filename: path,
       dumpLineNumbers: !this.optimize && this.config.dumpLineNumbers
     });
+
+    if (this.isIgnored(file.path)) return Promise.resolve(file);
 
     return less.render(data, config).then(output => {
       const data = output.css;
